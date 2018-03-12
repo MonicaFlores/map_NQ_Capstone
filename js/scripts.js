36352589ebeff108aacd081280f31b3ab0ad6e96
mf3435 <mf3435@nyu.edu>
2018-03-12 15:10:52 -0400

    add gardens click handler

--- js/scripts.js
+++ js/scripts.js
@@ -21,45 +21,83 @@ var CDsGeojson = L.geoJSON(CDs,{
           weight: 1.5,}
     }}).addTo(map);
 
-// Passed to onEachFeature: on click display info on side pane
-const ClickHandler = (e) => {
+		// To DO: polygon styles
+		//
+
+//Property title vacant land
+const getVacantTitle = (property) => {
+	$('#lot-address').append(`<b style='font-size: 15px'; 'font-weight: 150%'; font-family: 'Roboto Mono', sans-serif; >Vacant Lot</b>
+					at ${property.Address}.<br/>`);
+}
+
+const getGardenTitle = (property) => {
+	$('#lot-address').append(`<b style='font-size: 15px'; 'font-weight: 150%'; font-family: 'Roboto Mono', sans-serif; >${property.Name}</b> <br/>
+												${property.Garden_dev} Community Garden at ${property.Address}.<br/>`);
+}
+//Lot info for all lots (vacant and gardens)
+const getLotInfo = (property) => {
+	$('#lot-info').append(`
+			<b style='font-size: 120%'> Owner:</b> ${property.OwnerName}.<br/>
+			<b style='font-size: 120%'> Area:</b> ${numeral(property.LotArea).format('0,0')} sqft.<br/>
+			<b style='font-size: 120%'> Zoning:</b> ${property.ZoneDist1}. <b style='margin-left: 5px'> Overlay:</b> ${property.Overlay1}.<br/>
+			<b style='font-size: 120%'> FAR:</b>
+			<b>Residential: </b> ${property.ResidFAR};
+			<b style='margin-left: 5px'>Commercial: </b> ${property.CommFAR};
+			<b style='margin-left: 5px'>Facilities: </b> ${property.FacilFAR}.
+			<hr>
+			<b style='font-size: 120%'> Total Air Rights:</b><br/>
+			<b style='margin-left: 20px'>Residential: </b> ${numeral(property.ResidFAR * property.LotArea).format('0,0')} sqft.<br/>
+			<b style='margin-left: 20px'>Commercial: </b> ${numeral(property.CommFAR * property.LotArea).format('0,0')} sqft.<br/>
+			<b style='margin-left: 20px'>Facilities: </b> ${numeral(property.FacilFAR * property.LotArea).format('0,0')} sqft.<br/>
+			`);
+}
+
+//Get Panorama
+// const getPano = (layer) => {
+// 	const centerFeature = turf.centerOfMass(layer);
+// 	const center = centerFeature.geometry.coordinates;
+// 	const panoramaOptions = {
+// 		position: {
+// 			lat: center[1],
+// 			lng: center[0],
+// 		}
+// 	};
+// 	$('#pano').append(
+// 	setTimeout(function() {
+// 		new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions)
+// 	}, 100)
+// )}
+
+// Passed to onEachFeature: on click display info on side pane - vacant land
+const ClickHandlerVacant = (e) => {
   const layer = e.target;
 	const property = layer.feature.properties;
 	// Update Side Pane for clicked lot
 	expandSidePane();
-  // To DO: polygon styles
-	//
 	// // Fill in Property Info tab
-	// populatePane();
 	$('#lot-address').empty();
-	$('#lot-address').append(`<b style='font-size: 15px'; 'font-weight: 150%'; font-family: 'Roboto Mono', sans-serif; >Vacant Lot</b>
-					at ${property.Address}.<br/>`);
-
-	// streetview
+	getVacantTitle(property)
+	// Panorama
 	$('#pano').empty();
-
-	// Get panorama
-	// var centerFeature = turf.centerOfMass(e);
-	// var center = centerFeature.geometry.coordinates;
-	// var panoramaOptions = {
-	// 	position: {
-	// 		lat: center[1],
-	// 		lng: center[0],
-	// 	}
-	// };
-	// setTimeout(function() {
-	// 	new google.maps.StreetViewPanorama(document.getElementById('pano'), panoramaOptions)
-	// }, 100)
-
+	// getPano(layer)
 	$('#lot-info').empty();
-	$('#lot-info').append(`
-			<b style='font-size: 120%'> Owner:</b> ${property.OwnerName}.<br/>
-			<b style='font-size: 120%'> Area:</b> ${numeral(property.LotArea).format('0,0')} sqft.<br/>
-			<b style='font-size: 120%'> Zoning:</b> ${property.ZoneDist1}.<br/>
-			<b style='font-size: 120%'> Overlay:</b> ${property.Overlay1}.<br/>
-			<b style='font-size: 120%'> FAR:</b>  ${property.ResidFAR} residential;
-						${property.CommFAR} commercial;
-						${property.FacilFAR} facilities.<br/>`);
+	getLotInfo(property)
+};
+
+// Passed to onEachFeature: on click display info on side pane - garden
+const ClickHandlerGarden = (e) => {
+  const layer = e.target;
+	const property = layer.feature.properties;
+	// Update Side Pane for clicked lot
+	expandSidePane();
+	// // Fill in Property Info tab
+	$('#lot-address').empty();
+	getGardenTitle(property)
+	// Panorama
+	$('#pano').empty();
+	// getPano(layer)
+	$('#lot-info').empty();
+	getLotInfo(property)
 };
 
 // Function passed to L.geoJSON's 'style' option - vacant land
@@ -80,9 +118,17 @@ const vacantGardenStyles = (feature) => {
 		weight: 1,};
 	};
 
-const propertyActions = (feature, layer) => {
+const propertyActionsVacant = (feature, layer) => {
   layer.on({
-    'click': ClickHandler,
+    'click': ClickHandlerVacant,
+    // 'mouseover': highlightFeature,
+    // 'mouseout': resetHighlight
+	  });
+	};
+
+const propertyActionsGarden = (feature, layer) => {
+  layer.on({
+    'click': ClickHandlerGarden,
     // 'mouseover': highlightFeature,
     // 'mouseout': resetHighlight
 	  });
@@ -91,16 +137,16 @@ const propertyActions = (feature, layer) => {
 //Add data on vacant land
 var landGeojson = L.geoJSON(land, {
 	style: vacantLandStyles,
-	onEachFeature: propertyActions
+	onEachFeature: propertyActionsVacant
 	}).addTo(map);
 
 	// <b style='font-size: 15px'; 'font-weight: 150%'; font-family: 'Roboto Mono', sans-serif; >${feature.properties.Name}</b> <br/>
 	// 								${feature.properties.Garden_dev} Community Garden at ${feature.properties.Address}.<br/>
 
 //Add data on gardens
-var landGeojson = L.geoJSON(gardens, {
+var gardensGeojson = L.geoJSON(gardens, {
 	style: vacantGardenStyles,
-	onEachFeature: propertyActions
+	onEachFeature: propertyActionsGarden
 	}).addTo(map);
 
 
